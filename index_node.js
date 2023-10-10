59be886b92d61624147f5ad28a246277814bd651
Suuuuui <Gasidid.bobby.7@gmail.com>
2023-10-10 10:49:48 +0700

    kuyOAK

--- index_node.js
+++ index_node.js
@@ -1,20 +1,35 @@
 const express = require('express')
+const { register } = require('module')
 const router = express.Router()
 const app = express()
 const path = require('path')
 
 app.use(express.static('page_all/home_main'))
+app.use(express.static('page_all/Login_page'))
+app.use(express.static('page_all/Register'))
+
+
 app.use('/page_all/home_main',express.static('home_main'))
 
-const fuck = path.join(__dirname,"page_all/home_main/home.html")
 
-
+const home_page = path.join(__dirname,"page_all/home_main/home.html")
+const login_page = path.join(__dirname,"page_all/Login_page/loginhtml")
+const register_page = path.join(__dirname,"page_all/Register/register.html")
 
 router.get("/",(req,res)=>{
     res.status(200)
     res.type('text/html')
-    res.sendFile(fuck)
+    res.sendFile(home_page)
 })
+
+router.get("/login",(req,res)=>{
+    res.sendFile(login_page)
+})
+
+router.get("/register",(req,res)=>{
+    res.sendFile(register_page)
+})
+
 
 app.use(router)
 
