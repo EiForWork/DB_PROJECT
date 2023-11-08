f5814bb526d1b41a1f2345ded71e55bcb46cef64
Suuuuui <Gasidid.bobby.7@gmail.com>
2023-11-08 14:30:07 +0700

    kuy_get

--- Frontend/BissHotella/src/components/footer/Footer.jsx
+++ Frontend/BissHotella/src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './fo.css'
 import {createBrowserRouter,RouterProvider,Route,Link} from 'react-router-dom'
+import face from './foot_photos/face.png'
+import yt from './foot_photos/yt.png'
+import gm from './foot_photos/gmail.png'
+import line from './foot_photos/line.png'
+
 
 function Footer() {
   return (
@@ -13,6 +18,13 @@ function Footer() {
             <Link to='/contact'>CONTACT</Link>
             <Link to='/blog'>BLOG</Link>
         </nav>  
+        <div className="logo">
+          <img src={face}/>
+          <img src={yt}/>
+          <img src={gm}/>
+          <img src={line}/>
+        </div>
+        <p>Copyright &copy;2023 </p>
     </div>
     </>
   )
